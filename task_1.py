3fe5a925b74942a184025c62f6ad111c9d136e43
WariaX <115689237+WariaX@users.noreply.github.com>
2022-11-28 09:29:49 +0100

    Update task_1.py

--- task_1.py
+++ task_1.py
@@ -1,2 +1,7 @@
 def is_palindrome(text):
-    return False
+    text = text.lower().replace(" ", "")
+    n = len(text)
+    for i in range(n-1):
+        if text[i] != text[n-1-i]: 
+            return False 
+    return True;
