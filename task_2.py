3c5d127760f3fcb9f086c9242ff5069b15fa1619
WariaX <115689237+WariaX@users.noreply.github.com>
2022-12-09 10:48:11 +0100

    Update task_2.py

--- task_2.py
+++ task_2.py
@@ -2,20 +2,22 @@ def my_split(text):
     result = []
     czy_wyraz = True 
     wyraz = ""   
-           
+            # " Hobbit   ma kota"
     for i in range(len(text)):
-              
+        
         if text[i] ==" ":
             czy_wyraz = False  
 
-        if  text[i-1] ==" ": 
+        elif  text[i-1] ==" ": 
             czy_wyraz = True    
 
         if czy_wyraz == True:
             wyraz += text[i] 
 
-        else:
+        elif wyraz != "":
             result.append(wyraz) 
             wyraz = ""
-    result.append(wyraz)  
+
+    if wyraz != "":       
+        result.append(wyraz)  
     return result
