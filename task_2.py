6272b0a7f5ece38e78edcc9a448e2326a1b6ad27
WariaX <115689237+WariaX@users.noreply.github.com>
2022-12-09 10:31:28 +0100

    Update task_2.py

--- task_2.py
+++ task_2.py
@@ -1,2 +1,21 @@
-def my_split(text):
-    return [text]
+def my_split(text):    
+    result = []
+    czy_wyraz = True 
+    wyraz = ""   
+           
+    for i in range(len(text)):
+              
+        if text[i] ==" ":
+            czy_wyraz = False  
+
+        if  text[i-1] ==" ": 
+            czy_wyraz = True    
+
+        if czy_wyraz == True:
+            wyraz += text[i] 
+
+        else:
+            result.append(wyraz) 
+            wyraz = ""
+    result.append(wyraz)  
+    return result
